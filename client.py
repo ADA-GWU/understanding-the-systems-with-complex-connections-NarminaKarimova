2d4fb02d0c1de223ee0e0b57697038890fc12c76
NarminaKarimova <narmina.krmva@gmail.com>
2023-09-28 12:38:33 +0400

    managed to create infinite inputs

--- client.py
+++ client.py
@@ -1,6 +1,33 @@
 import socket
-s = socket.socket()
-port = 9001
-s.connect(('127.0.0.1', port))
-print((s.recv(1024)))
-s.close()
+
+# Function to connect to a server and send/receive data
+def connect_to_server(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('127.0.0.1', port))
+
+        while True:
+            # Input number
+            number = input(f"Enter a number (connected to port {port}, 'exit' to quit): ")
+            if number.lower() == 'exit':
+                break
+
+            # Send the number to the server
+            s.send(number.encode())
+
+            # Receive and print the server's response
+            response = s.recv(1024).decode()
+            print(f"Server response from port {port}: {response}")
+
+    except ConnectionRefusedError:
+        print(f"Error: Server on port {port} is not running. Please start the server.")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        s.close()
+
+if __name__ == "__main__":
+    ports = [9001]
+
+    for port in ports:
+        connect_to_server(port)
--- server.py
+++ server.py
@@ -1,14 +1,39 @@
 import socket
-s = socket.socket()
-print('Socket created')
-port = 9001
-s.bind(('',port))
-print(f'socket binded to port{port}')
-s.listen(3)
-print('Socket is listening')
-while True:
-    c, addr = s.accept()
-    print('Got connection from', addr)
-    message = ('Thanks for coonection')
-    c.send(message.encode())
+import sys
+
+# Function to handle a single client connection
+def handle_client(c):
+    while True:
+        try:
+            number = c.recv(1024).decode()
+            if not number:
+                break
+
+            # Double the input number
+            result = str(int(number) * 2)
+            print('Doubled number from client input:', result)
+            c.send(result.encode())
+        except ValueError:
+            c.send("Invalid input. Please enter a number.".encode())
     c.close()
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python server.py <port>")
+        return
+
+    port = int(sys.argv[1])
+    s = socket.socket()
+    s.bind(('', port))
+    print(f'Server bound to port: {port}')
+    s.listen(1)
+
+    print(f'Server is listening on port {port}')
+
+    while True:
+        c, addr = s.accept()
+        print('Connected to client:', addr)
+        handle_client(c)
+
+if __name__ == "__main__":
+    main()
